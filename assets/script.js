340654d6dbbf56c62d2d9796f08d8336f7a8472b
Gwack <gracalynntrammell@gmail.com>
2021-10-16 12:25:23 -0500

    html file styled and link added to view high scores. some variables created in java

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var startButton = document.querySelector("start-btn");
+
+var questionCounter = 0;
+var timer = document.getElementById("timer");
+var timeLeft = 60;
+
+// once quiz is started, timer starts counting down
+
+
+// when user clicks start buttion---
+startButton.addEventListener("click", function() {
+
+})
