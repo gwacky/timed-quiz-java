e4439a0985194db98226734f3f06e7a4be82f7cd
Gwack <gracalynntrammell@gmail.com>
2021-10-16 12:39:20 -0500

    optimized classes to ID's to make flow between files easier. Added variables used for quiz in java file

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,20 @@
-var startButton = document.querySelector("start-btn");
-
-var questionCounter = 0;
-var timer = document.getElementById("timer");
+var startButton = document.querySelector("#start-btn");
+var timer = document.querySelector("#timer");
+var timeInterval = 0;
 var timeLeft = 60;
+var penalty = 10;
+var score = 0;
+var questionsDiv = document.querySelector("#questionsDiv");
+var questionIndex = 0;
+var unorderedList = document.querySelector("#unorderedList");
+var highScore = document.querySelector("#highscores");
+var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#goBack");
 
 // once quiz is started, timer starts counting down
+var countdown = function() {
 
+}
 
 // when user clicks start buttion---
 startButton.addEventListener("click", function() {
